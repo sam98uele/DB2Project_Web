826e9e3176d0623cdaa944b9e6d900d58bfaec75
Mattia <mattiapor@gmail.com>
2021-03-07 02:52:30 +0100

    not complete admin inspection

--- src/it/polimi/db2/project/controllers/UsersQuestionnaireInspection.java
+++ src/it/polimi/db2/project/controllers/UsersQuestionnaireInspection.java
@@ -1,6 +1,9 @@
 package it.polimi.db2.project.controllers;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletContext;
@@ -15,28 +18,29 @@ import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
+import it.polimi.db2.project.entities.Product;
+import it.polimi.db2.project.entities.User;
 import it.polimi.db2.project.services.ProductService;
 import it.polimi.db2.project.services.QuestionnaireAdminService;
-import it.polimi.db2.project.services.UserService;
 
 /**
- * Servlet implementation class Inspection
+ * Servlet implementation class UsersQuestionnaireInspection
  */
-@WebServlet("/Inspection")
-public class Inspection extends HttpServlet {
+@WebServlet("/UsersQuestionnaireInspection")
+public class UsersQuestionnaireInspection extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private TemplateEngine templateEngine;
+	private TemplateEngine templateEngine;   
 	
 	@EJB(name = "it.polimi.db2.project.services/QuestionnaireAdminService")
 	private QuestionnaireAdminService questAdminSer;
 	
 	@EJB(name = "it.polimi.db2.project.services/ProductService")
 	private ProductService prodSer;
-       
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Inspection() {
+    public UsersQuestionnaireInspection() {
         super();
     }
     
@@ -53,14 +57,37 @@ public class Inspection extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = "/WEB-INF/Inspection.html";
+		String path = "/WEB-INF/UsersQuestionnaireInspection.html";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		
+		Integer ID = null;
 		try {
-		ctx.setVariable("pastProd", prodSer.getPastScheduledProductOfTheDay());
+			ID = Integer.parseInt(request.getParameter("ID")); 
 		}catch(Exception e) {
-			//To handle
+			//Do nothing
 		}
+
+
+		try {
+			ctx.setVariable("subUsers", questAdminSer.getListUserCompiledQuestionnaire(ID, true));
+			ctx.setVariable("delUsers", questAdminSer.getListUserCompiledQuestionnaire(ID, false));
+		}catch(Exception e) {
+			//TODO To handle query exception
+			e.printStackTrace();
+		}
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		String date = sdf.format(prodSer.getProductById(ID).getDate());
+		
+		try {
+			ctx.setVariable("name", prodSer.getProductById(ID).getName());
+			ctx.setVariable("date", date);
+		}catch(Exception e) {
+			e.printStackTrace();
+			//TODO handle
+		}
+		
 		templateEngine.process(path, ctx, response.getWriter());
 	}
 
